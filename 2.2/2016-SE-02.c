b3a3d9f4995e1c9bcf7c12c3808d6c44ced7fb90
Georgi Petrov <85412257+Gergi3@users.noreply.github.com>
2026-06-17 15:54:50 +0300

    Create 2016-SE-02.c

---
+++ 2.2/2016-SE-02.c
@@ -0,0 +1,48 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <stdbool.h>
+#include <string.h>
+#include <err.h>
+#include <stdlib.h>
+
+#define FORK_ERR 100, "Couldn't fork"
+#define EXEC_ERR 101, "Couldn't exec command"
+#define WRITE_ERR 102, "Couldn't write"
+#define READ_ERR 103, "Couldn't read"
+
+#define PROMPT_MSG "Prompt: "
+#define BUF_SIZE 4096
+void sys_err(int status, int code, const char* message) {
+    if (status == -1) {
+        err(code, message);
+    }
+}
+
+void write_s(int fd, const void* buf, size_t len) {
+    sys_err(write(fd, buf, len), WRITE_ERR);
+}
+
+size_t read_s(int fd, void* buf, size_t len) {
+    ssize_t read_bytes = read(fd, buf, len);
+    sys_err(read_bytes, READ_ERR);
+    return (size_t)read_bytes;
+}
+
+int main(void) {
+    while (true) {
+        write_s(STDOUT_FILENO, &PROMPT_MSG, strlen(PROMPT_MSG)); 
+        
+        char cmd[BUF_SIZE];
+        size_t read = read_s(STDIN_FILENO, cmd, sizeof(cmd));
+        cmd[read - 1] = '\0';
+        if (strcmp(cmd, "exit") == 0) {
+            exit(0);
+        }
+
+        pid_t child_pid = fork();
+        if (child_pid == 0) {
+            sys_err(execlp(cmd, cmd, NULL), EXEC_ERR);
+        }
+    }
+}
