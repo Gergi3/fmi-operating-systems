1472c1b53457cb132f82ca2c8f96ca8378e650b6
Georgi Petrov <85412257+Gergi3@users.noreply.github.com>
2026-06-18 11:28:02 +0300

    Create 2020-SE-01-foo.c

---
+++ 2.2/2020-SE-01-foo.c
@@ -0,0 +1,60 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <err.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <sys/wait.h>
+#include <errno.h>
+
+#define ARGS_ERR 100, "Invalid args"
+#define MKFIFO_ERR 101, "Couldn't create a named pipe"
+#define EXEC_ERR 102, "Couldn't execute command"
+#define READ_ERR 104, "Couldn't read"
+#define WRITE_ERR 105, "Couldn't write"
+#define OPEN_ERR 106, "Couldn't open fd"
+#define DUP_ERR 107, "Couldn't dup fds"
+#define UNLINK_ERR 108, "Couldn't delete old pipe"
+
+#define REQUIRED_ARGS 2
+#define FIFO_NAME "somefifo"
+
+void sys_err(int status, int code, const char* message);
+int mkfifo_s(int flags);
+void assert_true(bool success, int code, const char* message);
+
+void sys_err(int status, int code, const char* message) {
+    if (status == -1) {
+        err(code, message);
+    }
+}
+
+void assert_true(bool success, int code, const char* message) {
+    if (!success) {
+        errx(code, message);
+    }
+}
+
+int mkfifo_s(int flags) {
+    if (unlink(FIFO_NAME) == -1 && errno != ENOENT) {
+        err(UNLINK_ERR);
+    };
+
+    sys_err(mkfifo(FIFO_NAME, S_IRUSR | S_IWUSR), MKFIFO_ERR);
+    int fd = open(FIFO_NAME, flags);
+    sys_err(fd, OPEN_ERR);
+    return fd;
+}
+
+int main(int argc, char** argv) {
+    assert_true(argc == REQUIRED_ARGS, ARGS_ERR);
+    
+    int fd = mkfifo_s(O_WRONLY);
+    sys_err(dup2(fd, STDOUT_FILENO), DUP_ERR);    
+    close(fd);
+
+    const char* file = argv[1];
+    int err = execlp("cat", "cat", file, NULL);
+    sys_err(err, EXEC_ERR);
+}
